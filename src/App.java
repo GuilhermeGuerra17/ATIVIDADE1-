1c0129c08cefe514b5ce0da537b0519f457ce5d7
Guilherme Guerra Ferreira <guiferreira22@yahoo.com.br>
2022-10-03 22:55:32 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner (System.in);
+        int numero;
+        System.out.println("Informe um numero: ");
+       numero = entrada.nextInt(); 
+       System.out.println("O numero informado foi: "+numero);
+        
 
 
     }
